4c7297f8058ae5bf63a9995db430fa94fefbb107
Jesse Falconer <jesse.falconer@spark.re>
2020-12-01 13:24:47 -0800

    Sort and exit current loop when sum begins exceeding 2020

--- 1b.cpp
+++ 1b.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 #define is2020(x) (x == 2020)
+#define exceeds2020(x) (x > 2020)
 
 int add (int x, int y, int z) {
   int sum;
@@ -26,17 +28,29 @@ int main()
     675,
     1456
   };
+  std::sort(report.begin(), report.end());
+
   int maxSize = report.size();
 
   for (int x=0; x<maxSize-2; x++)
-    for (int y=x+1; y<maxSize-1; y++)
-      for (int z=y+1; z<maxSize; z++)
-      {
+    for (int y=x+1; y<maxSize-1; y++) {
         int sum;
-        sum = add(report[x], report[y], report[z]);
-        if (is2020(sum))
-        {
-          cout << multiply(report[x], report[y], report[z]);
+        sum = add(report[x], report[y], 0);
+
+        if (exceeds2020(sum)) {
+          break;
+        }
+
+        for (int z=y+1; z<maxSize; z++) {
+          int sum;
+          sum = add(report[x], report[y], report[z]);
+
+          if (is2020(sum)) {
+            cout << multiply(report[x], report[y], report[z]);
+            }
+            else if (exceeds2020(sum)) {
+              break;
+            }
         }
       }
 }
